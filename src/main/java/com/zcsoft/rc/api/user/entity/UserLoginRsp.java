ff48a8769237a383743b86c9902e544203a7c2be
joly001 <joly_huang@163.com>
2019-04-01 11:13:38 +0800

    init

--- src/main/java/com/zcsoft/rc/api/user/entity/UserLoginRsp.java
+++ src/main/java/com/zcsoft/rc/api/user/entity/UserLoginRsp.java
@@ -18,6 +18,18 @@ public class UserLoginRsp {
      * 手机
      */
     private String mobile;
+    /**
+     * 用户图片
+     */
+    private String userPicture;
+    /**
+     * 组织名称
+     */
+    private String orgName;
+    /**
+     * 施工人员类型
+     */
+    private String builderUserType;
 
     public String getNick() {
         return nick;
@@ -51,6 +63,30 @@ public class UserLoginRsp {
         this.mobile = mobile;
     }
 
+    public String getUserPicture() {
+        return userPicture;
+    }
+
+    public void setUserPicture(String userPicture) {
+        this.userPicture = userPicture;
+    }
+
+    public String getOrgName() {
+        return orgName;
+    }
+
+    public void setOrgName(String orgName) {
+        this.orgName = orgName;
+    }
+
+    public String getBuilderUserType() {
+        return builderUserType;
+    }
+
+    public void setBuilderUserType(String builderUserType) {
+        this.builderUserType = builderUserType;
+    }
+
     @Override
     public String toString() {
         final StringBuilder sb = new StringBuilder("UserLoginRsp{");
@@ -58,6 +94,9 @@ public class UserLoginRsp {
         sb.append(", wristStrapCode='").append(wristStrapCode).append('\'');
         sb.append(", loginToken='").append(loginToken).append('\'');
         sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", userPicture='").append(userPicture).append('\'');
+        sb.append(", orgName='").append(orgName).append('\'');
+        sb.append(", builderUserType='").append(builderUserType).append('\'');
         sb.append('}');
         return sb.toString();
     }
