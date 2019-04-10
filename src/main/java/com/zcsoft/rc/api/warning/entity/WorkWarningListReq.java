171c371407da8a73d741bd6ba334789e61798610
joly001 <joly_huang@163.com>
2019-04-10 19:03:16 +0800

    init

--- src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListReq.java
+++ src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListReq.java
@@ -1,91 +1,24 @@
 package com.zcsoft.rc.api.warning.entity;
 
-import java.util.Date;
-
 public class WorkWarningListReq {
 
     /**
-     * id
+     * 作业面id
      */
-    private String id;
-    /**
-     * 里程区间名称
-     */
-    private String mileageSegmentName;
-    /**
-     * 昵称
-     */
-    private String nick;
-    /**
-     * 手机
-     */
-    private String mobile;
-    /**
-     * 类型(00:接近警告线)
-     */
-    private String type;
-    /**
-     * 创建时间
-     */
-    private Date createTime;
+    private String workSegmentId;
 
-    public String getId() {
-        return id;
+    public String getWorkSegmentId() {
+        return workSegmentId;
     }
 
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getMileageSegmentName() {
-        return mileageSegmentName;
-    }
-
-    public void setMileageSegmentName(String mileageSegmentName) {
-        this.mileageSegmentName = mileageSegmentName;
-    }
-
-    public String getNick() {
-        return nick;
-    }
-
-    public void setNick(String nick) {
-        this.nick = nick;
-    }
-
-    public String getMobile() {
-        return mobile;
-    }
-
-    public void setMobile(String mobile) {
-        this.mobile = mobile;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
+    public void setWorkSegmentId(String workSegmentId) {
+        this.workSegmentId = workSegmentId;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("WorkWarningListReq{");
-        sb.append("id='").append(id).append('\'');
-        sb.append(", mileageSegmentName='").append(mileageSegmentName).append('\'');
-        sb.append(", nick='").append(nick).append('\'');
-        sb.append(", mobile='").append(mobile).append('\'');
-        sb.append(", type='").append(type).append('\'');
-        sb.append(", createTime=").append(createTime);
+        final StringBuilder sb = new StringBuilder("WorkWarningListRsp{");
+        sb.append("workSegmentId='").append(workSegmentId).append('\'');
         sb.append('}');
         return sb.toString();
     }
--- src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListRsp.java
+++ src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListRsp.java
@@ -1,24 +1,91 @@
 package com.zcsoft.rc.api.warning.entity;
 
+import java.util.Date;
+
 public class WorkWarningListRsp {
 
     /**
-     * 作业面id
+     * id
      */
-    private String workSegmentId;
+    private String id;
+    /**
+     * 里程区间名称
+     */
+    private String mileageSegmentName;
+    /**
+     * 昵称
+     */
+    private String nick;
+    /**
+     * 手机
+     */
+    private String mobile;
+    /**
+     * 类型(00:接近警告线)
+     */
+    private String type;
+    /**
+     * 创建时间
+     */
+    private Date createTime;
 
-    public String getWorkSegmentId() {
-        return workSegmentId;
+    public String getId() {
+        return id;
     }
 
-    public void setWorkSegmentId(String workSegmentId) {
-        this.workSegmentId = workSegmentId;
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMileageSegmentName() {
+        return mileageSegmentName;
+    }
+
+    public void setMileageSegmentName(String mileageSegmentName) {
+        this.mileageSegmentName = mileageSegmentName;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
     }
 
     @Override
     public String toString() {
-        final StringBuilder sb = new StringBuilder("WorkWarningListRsp{");
-        sb.append("workSegmentId='").append(workSegmentId).append('\'');
+        final StringBuilder sb = new StringBuilder("WorkWarningListReq{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", mileageSegmentName='").append(mileageSegmentName).append('\'');
+        sb.append(", nick='").append(nick).append('\'');
+        sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", createTime=").append(createTime);
         sb.append('}');
         return sb.toString();
     }
