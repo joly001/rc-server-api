4ae11d408ebbd803a8e56eb753dd324afb8a85ac
joly001 <joly_huang@163.com>
2019-04-10 17:43:37 +0800

    init

--- src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListReq.java
+++ src/main/java/com/zcsoft/rc/api/warning/entity/WorkWarningListReq.java
@@ -13,30 +13,6 @@ public class WorkWarningListReq {
      */
     private String mileageSegmentName;
     /**
-     * 用户id			db_column: USER_ID
-     */
-    private String userId;
-    /**
-     * 施工人员类型(00:机车、01:列车、02:作业人员、03:作业负责人、04:安全员、05:防护员、06:监理、07:其它人员)			db_column: BUILDER_USER_TYPE
-     */
-    private String builderUserType;
-    /**
-     * 部门id			db_column: DEP_ID
-     */
-    private String depId;
-    /**
-     * 部门名称			db_column: DEP_NAME
-     */
-    private String depName;
-    /**
-     * 组织id			db_column: ORG_ID
-     */
-    private String orgId;
-    /**
-     * 组织名称			db_column: ORG_NAME
-     */
-    private String orgName;
-    /**
      * 昵称			db_column: NICK
      */
     private String nick;
@@ -45,20 +21,72 @@ public class WorkWarningListReq {
      */
     private String mobile;
     /**
-     * 状态(CREATE:创建、FINISH:结束)			db_column: STATUS
-     */
-    private String status;
-    /**
-     * 类型(00:接近警告线)			db_column: TYPE
+     * 类型(00:接近警告线)
      */
     private String type;
     /**
-     * 创建时间			db_column: CREATE_TIME
+     * 创建时间
      */
     private Date createTime;
-    /**
-     * 修改时间			db_column: MODIFY_TIME
-     */
-    private Date modifyTime;
 
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getMileageSegmentName() {
+        return mileageSegmentName;
+    }
+
+    public void setMileageSegmentName(String mileageSegmentName) {
+        this.mileageSegmentName = mileageSegmentName;
+    }
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getMobile() {
+        return mobile;
+    }
+
+    public void setMobile(String mobile) {
+        this.mobile = mobile;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public Date getCreateTime() {
+        return createTime;
+    }
+
+    public void setCreateTime(Date createTime) {
+        this.createTime = createTime;
+    }
+
+    @Override
+    public String toString() {
+        final StringBuilder sb = new StringBuilder("WorkWarningListReq{");
+        sb.append("id='").append(id).append('\'');
+        sb.append(", mileageSegmentName='").append(mileageSegmentName).append('\'');
+        sb.append(", nick='").append(nick).append('\'');
+        sb.append(", mobile='").append(mobile).append('\'');
+        sb.append(", type='").append(type).append('\'');
+        sb.append(", createTime=").append(createTime);
+        sb.append('}');
+        return sb.toString();
+    }
 }
